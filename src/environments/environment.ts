b731029c72a027fa5fa18c1bbc7226f89d610c0a
2371857_cgcp <Venkateswaramma.Vasapilli@cognizant.com>
2026-06-01 21:38:51 +0530

    changed url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://crm-api-vev-bsfte9h7dsarb6es.southindia-01.azurewebsites.net/api',
+  apiBaseUrl: 'https://crm-api-vev-dsbtc0gtfvfvahec.southindia-01.azurewebsites.net/api',
 };
